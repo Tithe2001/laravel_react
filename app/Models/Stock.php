4bd180387b0210f958c9a76e680a311f955700f9
Tithe2001 <afrinakthertithebhuiyan@gmail.com>
2026-01-29 12:54:13 +0600

    stock update

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -6,10 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Stock extends Model
 {
+
+protected $table = 'stocks';
+
+    protected $fillable = [
+        'product_id',
+        'quantity',
+        'transaction_type_id',
+        'warehouse_id',
+    ];
+
       public function product(){
 
     return $this->belongsTo(Product::class,"product_id");
     }
 
-    
 }
